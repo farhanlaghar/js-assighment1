087f24bee13deffd56da44d11634c821eb51c282
Farhan-Laghari <157783005+farhanlaghar@users.noreply.github.com>
2024-09-11 17:08:44 +0500

    Update app3.js

---
+++ app3.js
@@ -0,0 +1,52 @@
+                     // VARIABLES FOR NUMBERS
+ //*--------------------------------**----------------------------*
+
+
+
+// Qno 01
+// 1. Declare a variable called age & assign to it your age. Show
+// your age in an alert box.
+// var age = 25; 
+// alert("Your age is: " + age);
+//*--------------------------------**----------------------------*
+
+
+
+
+// Qno 02
+// Declare & initialize a variable to keep track of how many
+// times a visitor has visited a web page. Show his/her
+// number of visits on your web page. For example: “You
+// have visited this site N times”.
+// var visitCount = 5; 
+// document.write( "You have visited this site " + visitCount + " times.")
+//*--------------------------------**----------------------------*
+
+
+// Qno 03
+// 3. Declare a variable called birthYear & assign to it your
+// birth year. Show the following message in your browser:
+// var birthYear = 1990;  
+// var message = "Your birth year is: " + birthYear;
+// document.write(message);
+//*--------------------------------**----------------------------*
+
+
+
+
+
+// Qno 04
+// 4. A visitor visits an online clothing store
+// www.xyzClothing.com . Write a script to store in variables
+// the following information:
+// a. Visitor’s name
+// b. Product title
+// c. Quantity i.e. how many products a visitor wants to order
+// Show the following message in your browser: “John
+// Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+// var visitorName = "John Doe";
+// var productTitle = "T-shirt";
+// var quantity = 5;
+// var message = visitorName + " ordered " + quantity + " " + productTitle + "(s) on XYZ Clothing store.";
+// document.write(message);
+//*--------------------------------**----------------------------*
