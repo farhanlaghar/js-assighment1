50f622e45487ac187c6e5c9b674ddae1aef79e7b
Farhan-Laghari <157783005+farhanlaghar@users.noreply.github.com>
2024-09-11 17:06:44 +0500

    Update app2.js

---
+++ app2.js
@@ -0,0 +1,90 @@
+ // Assignment no 02
+            //    VARIABLES FOR STRINGS
+            //*----------------------**--------------------------*
+
+// Qno 01
+// 1. Declare a variable called username.
+// var myName;
+  //*----------------------**--------------------------*
+
+
+// Qno 02
+// Declare a variable called myName & assign to it a string that represents your Full Name.
+// var myName = "Asad Abbas";
+  //*----------------------**--------------------------*
+
+
+
+// Qno 03
+// Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+// var message;
+// message = "Hello World";
+// alert(message);
+  //*----------------------**--------------------------*
+
+
+
+
+// Qno 04
+// Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+// var name = "Asad"
+// var age = "20 years old"
+// var certificate = "Certificate web and Mobile development";
+// alert(name)
+// alert(age)
+// alert(certificate)
+  //*----------------------**--------------------------*
+
+
+
+
+// Qno 05
+// 5. Write a script to display the following alert using one JS variable:
+// var message = "Pizza\npizz\npiz\npi\np";
+// alert(message)
+  //*----------------------**--------------------------*
+
+
+
+
+// Qno 06
+// Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+// Declare a variable called email and assign your email address to it
+// var email = "example@example.com";
+// var message = "My email address is: " + email;
+// alert(message);
+  //*----------------------**--------------------------*
+
+
+
+
+
+
+// Qno 07
+// 7. Declare a variable called book & give it the value “A
+// smarter way to learn JavaScript”. Display the following
+// message in an alert box:
+// Declare a variable called book and assign it a value
+// var book = "A smarter way to learn JavaScript";
+// var message = "The book title is: " + book;
+// alert(message);
+  //*----------------------**--------------------------*
+
+
+
+
+
+// Qno 08
+// 8. Write a script to display this in browser through JS
+// var message = "The book title is: A smarter way to learn JavaScript";
+// document.write(message)
+  //*----------------------**--------------------------*
+
+
+
