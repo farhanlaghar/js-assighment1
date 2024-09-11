96c1106ef41808138afe0e55fd1a4a3a34c0f216
Farhan-Laghari <157783005+farhanlaghar@users.noreply.github.com>
2024-09-11 17:05:53 +0500

    Update app1.js

---
+++ app1.js
@@ -0,0 +1,83 @@
+                             // Assignment no 01
+                                  //   ALERT
+
+
+//  Qno: 01
+// 1.Write a script to greet your website visitor using JS alert box.
+//  alert("Welcome to our website");
+//------------------*-----------------------------------------**
+
+
+//  Qno: 02
+// 2. Write a script to display following message on your web page.
+// document.write("Please Enter a Valid Password");
+//------------------*-----------------------------------------**
+
+
+
+//  Qno: 03
+//3. Write a script to display following message on your web page: (Hint : Use line break)
+// document.write("Welcome to JS land... <br> Happy Coding!");
+//------------------*-----------------------------------------**
+
+
+
+//   Qno: 04
+// 4. Write a script to display following messages in sequence:
+// alert("Message 1: Welcome to our website!");
+// alert("Message 2: We hope you find what you're looking for.");
+// alert("Message 3: Feel free to explore and contact us if you need help.");
+//------------------*-----------------------------------------**
+
+
+
+//   Qno: 05
+// 5. Generate the following message through browser’s developer console:
+// console.log("Message 1: Welcome to our website!");
+// console.log("Message 2: We hope you find what you're looking for.");
+// console.log("Message 3: Feel free to explore and contact us if you need help.");
+
+
+
+
+//   Qno: 06
+// Make use of alerts in your new/existing HTML & CSS project.
+
+//  a:Head Section
+{/* <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Script in Head</title>
+    <!-- Link to the external JavaScript file -->
+    <script src="script.js"></script>
+</head> */}
+
+// b):Body (Before Your Page’s HTML)
+// <!DOCTYPE html>
+// <script src="script.js"></script>
+// <html lang="en">
+// <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <title>Script Before HTML</title>
+// </head>
+// <body></body>
+
+// c): Body (Inside Your Page’s HTML)
+{/* <body>
+    <h1>Inside Body Example</h1>
+    <p>This page uses an external JavaScript file linked inside the body.</p>
+    
+    <!-- Link to the external JavaScript file -->
+    <script src="script.js"></script>
+</body> */}
+
+// d): Body (After Your Page’s HTML)
+// </head>
+// <body>
+//     <h1>After Body Example</h1>
+//     <p>This page uses an external JavaScript file linked after the body section.</p>
+// </body>
+// <!-- Link to the external JavaScript file -->
+// <script src="script.js"></script>
+// </html>
